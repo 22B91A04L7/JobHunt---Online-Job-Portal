f8770a4d0314840df26b0f41eb13a69dabb98d44
Venkat Sunkara <venkatsunkara9959@gmail.com>
2026-01-02 16:13:31 +0530

    Fix CORS configuration for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -20,7 +20,12 @@ await connectCloudinary();
 
 
 // Middleware to parse JSON bodies
-app.use(cors());
+app.use(cors({
+  origin: ['https://online-job-portal-client.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(clerkMiddleware());
 
